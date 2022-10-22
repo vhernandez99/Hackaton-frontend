93b2894412eccf497692b9cf652adae6f0365ee6
Francisco De Jesus Orta Ramirez <franpak2000@hotmail.com>
2022-10-22 16:24:34 -0500

    commit final front

--- components/Index.js
+++ components/Index.js
@@ -2,19 +2,19 @@ import Image from "next/image";
 import Logo from  "../assets/images/TICKERS1X1-01.png";
 const Index = () => {
     return ( 
-        <div className="flex items-center">
-            <div className="w-1/2 pl-20 ">
-                <div className="text-[4rem]">
+        <div className="flex items-center lgmax:flex-col">
+            <div className="w-1/2 pl-20 lgmax:w-full lgmax:pl-2 lgmax:pr-3">
+                <div className="text-[4rem] lgmax:text-[2rem]">
                     <h1>Descrubre Las</h1>
                     <h1>Las Mejores</h1>
                     <h1>Recompenzas De Nft</h1>
                 </div>
-                <div className="w-[65%] mt-8">
+                <div className="w-[65%] mt-8 lgmax:w-full">
                     <p className="mb-6">Aute anim laborum veniam labore cillum eu aliqua labore incididunt sunt cillum.Aute anim laborum veniam labore cillum eu aliqua labore incididunt sunt cillum.</p>
                     <a className="bg-[#222222] text-white py-2 px-12 rounded-lg">Descrubre</a>
                 </div>
             </div>
-            <div className="w-1/2 py-4 px-16">
+            <div className="w-1/2 lgmax:w-full py-4 px-16">
                 <Image src={Logo}/>
             </div>
         </div>
--- components/Team.js
+++ components/Team.js
@@ -21,21 +21,21 @@ const Team = () => {
 
     return ( 
         <>
-            <div className='mx-[20%]'>
+            <div className='mx-[20%] lgmax:mx-[5%]'>
                 <div className='flex flex-col justify-center items-center'>
-                    <h1 className='text-[4rem] font-extralight mb-6'> MEET THE TEAM </h1>
+                    <h1 className='text-[4rem] font-extralight mb-6 lgmax:text-[2rem] lgmax:mt-8'> MEET THE TEAM </h1>
                     <FontAwesomeIcon className='text-[3rem]' icon={faGlobe}/>
                 </div>
                 <div className='mt-8 space-y-6 mb-4'>
                     {
                         members && members.map((member, index) => (
-                            <div key={index} data-aos="zoom-in-up" className='flex border-t-blue-600 border-t px-4 pt-2'>
-                                <div className='w-1/2 text-xl'>
+                            <div key={index} data-aos="zoom-in-up" className='flex border-t-blue-600 border-t px-4 pt-2 lgmax:flex-col'>
+                                <div className='w-1/2 text-xl lgmax:w-full lgmax:flex lgmax:flex-col lgmax:items-justify-center'>
                                     <h2>{`${member.title} ${member.name}`}</h2>
                                     <p> {member.role} </p>
                                     <Image alt={member.name} className='bg-blue-600 rounded-lg' width={150} height={150} src={member.image}/>
                                 </div>
-                                <div className='w-1/2 space-y-8 text-xl'>
+                                <div className='w-1/2 space-y-8 text-xl lgmax:w-full lgmax:space-y-4 lgmax:text-sm lgmax:mt-2'>
                                     <p className=''> <span><a href=''>@tupapacrosfit</a></span> Nearly two decades in Business </p>
                                     <p> Ranging from nightclub ownership and textiles management, to PR, marketing and movie production.</p>
                                     <p> Known as the celebrities go-to man, Leon has a top-notch, international network in all fields.</p>
