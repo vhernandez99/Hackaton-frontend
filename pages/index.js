0d4d21049e66ba58943055f9109b629629d4d95a
Victor hernandez <vhernandez.99@hotmail.com>
2022-10-23 07:01:29 -0500

    vercel error

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-import { Banner, Empresas, Footer, Info, Main, Team, UserGrid, Welcome } from '../components'
+import Banner from "../components/Banner"
+import Empresas from "../components/Empresas"
+import Footer from "../components/Footer"
+import Info from "../components/Info"
+import Main from "../components/Main"
+import Team from "../components/Team"
+import UserGrid from "../components/UserGrid"
+import Welcome from "../components/Welcome"
+
 const index = () => {
     return (
         <>
