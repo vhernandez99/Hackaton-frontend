cb570ab5d837b3e882d9f8b7cc0832dbb588e0cc
Victor hernandez <vhernandez.99@hotmail.com>
2022-10-23 04:56:46 -0500

    new header

--- components/InterSystem/Header.js
+++ components/InterSystem/Header.js
@@ -1,17 +1,18 @@
-import { Button } from './Button'
+import { Button } from "./Button";
 
 export const Header = () => {
-    return (
-        <div className='flex justify-between items-center px-4 bg-blue-900 h-16'>
-            <div>
-                <span>Crypto Rewards</span>
-            </div>
-            <div className='flex justify-evenly w-[30rem] items-center'>
-                <Button text='Crear nuevo'  primary/>
-                <Button text='Registrar uso' primary/>
-                <Button text='Intercambios' primary />
-                <Button text='Salir' primary />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-between items-center px-4 bg-blue-900 h-16">
+      <div>
+        <span>Crypto Rewards</span>
+      </div>
+      <div className="flex justify-evenly w-[30rem] items-center">
+        <Button text="Inicio" primary />
+        <Button text="Crear nuevo" primary />
+        <Button text="Registrar uso" primary />
+        <Button text="Intercambios" primary />
+        <Button text="Salir" primary />
+      </div>
+    </div>
+  );
+};
